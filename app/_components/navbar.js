5b2e89dad2686d8a1a2a913cc1c74a728e809a84
Dat Do-Nguyen <dat.donguyen@edu.sait.ca>
2025-12-14 15:06:39 -0700

    added sign in, logout, profile pic and conditional statements

--- app/_components/navbar.js
+++ app/_components/navbar.js
@@ -1,10 +1,23 @@
+"use client";
 import Link from "daisyui/components/link";
 import Image from "next/image";
 import { useUserAuth } from "@/_utils/auth-context";
+import { useRouter } from "next/navigation";
 
 export default function NavBar() {
   // work on making the profile image on navbar persist throughout the website
-  const { user } = useUserAuth();
+  const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const router = useRouter();
+
+  async function handleSignOut() {
+    try {
+      await firebaseSignOut();
+      console.log("signing out");
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="ml-2 mt-5 mb-5 mr-2">
@@ -21,41 +34,40 @@ export default function NavBar() {
         </a>
       </div>
 
-      <div className="flex gap-10">
-        <div className="dropdown dropdown-end">
-          <div
-            tabIndex={0}
-            role="button"
-            className="btn btn-ghost btn-circle avatar"
-          >
-            <div className="w-10 rounded-full">
-              <img
-                alt="Tailwind CSS Navbar component"
-                src={
-                  "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                }
-              />
+      {user ? (
+        <div className="flex gap-10">
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src={user.photoURL} />
+              </div>
             </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a
+                  className="justify-between"
+                  onClick={() => router.push("/profile")}
+                >
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a onClick={handleSignOut}>Logout</a>
+              </li>
+            </ul>
           </div>
-          <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
         </div>
-      </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
